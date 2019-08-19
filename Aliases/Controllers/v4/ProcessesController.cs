1991981e55cdc59a920923491bcec323bd094ead
Stewart Pavitt <stewart.pavitt@gmail.com>
2019-08-19 14:39:43 +0100

    Created V4

---
+++ Aliases/Controllers/v4/ProcessesController.cs
@@ -0,0 +1,48 @@
+﻿extern alias v4Schemas;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StorageRepository;
+using System.Collections.Generic;
+using System.Linq;
+using v4Schemas::Schemas;
+
+namespace Aliases.Controllers.v4
+{
+    [ApiVersion("4.0", Deprecated = true)]
+    [Route("api/v4/processes")]
+    [ApiController]
+    public class ProcessesController : ControllerBase
+    {
+
+        private IDefaultStorageRepository _repo;
+
+        public ProcessesController(IDefaultStorageRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [MapToApiVersion("4.0")]
+        [HttpPost]
+        public ActionResult Write([FromBody] ProcessSchema schema)
+        {
+            _repo.Write(new v1StorageModel.StorageModel() { Contents = schema.ToString(), Id = schema.Id, VersionOfContents = 4 });
+            return Ok();
+        }
+
+
+        [MapToApiVersion("4.0")]
+        [HttpGet]
+        public ActionResult<List<ProcessSchema>> Get()
+        {
+            return _repo.GetAll().Select(x => new ProcessSchema(x.Contents)).ToList<ProcessSchema>();
+        }
+
+        [HttpGet]
+        [MapToApiVersion("4.0")]
+        [Route("{id}")]
+        public ActionResult<ProcessSchema> GetById(string id)
+        {
+            return new ProcessSchema(_repo.Get(id).Contents);
+        }
+    }
+}
---
+++ v4Models/Actor.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Schemas
+{
+    public class Actor
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+        public string Version = "v4";
+
+        public Actor()
+        {
+        }
+
+        public Actor(string name, string age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Actor(string potentialContents)
+        {
+            var actor = JsonConvert.DeserializeObject<Actor>(potentialContents);
+            this.Name = actor.Name;
+            this.Age = actor.Age;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
