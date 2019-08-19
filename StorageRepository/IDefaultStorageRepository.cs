1d9a134788df94fc9694f7421df3ef70301eb141
Stewart Pavitt <stewart.pavitt@gmail.com>
2019-08-19 13:02:57 +0100

    Created a V1 of the project without external aliases

---
+++ StorageRepository/IDefaultStorageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using v1StorageModel;
+
+namespace StorageRepository
+{
+    public interface IDefaultStorageRepository
+    {
+        StorageModel Get(string id);
+        List<StorageModel> GetAll();
+        void Write(StorageModel model);
+    }
+}
---
+++ v1Models/ProcessSchema.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Schemas
+{
+    public class ProcessSchema
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Version = "v1";
+
+        public ProcessSchema()
+        {
+        }
+
+        public ProcessSchema(string potentialContents )
+        {
+            var schema = JsonConvert.DeserializeObject<ProcessSchema>(potentialContents);
+            this.Id = schema.Id;
+            this.Title = schema.Title;
+            this.Version = schema.Version;
+        }
+
+        public ProcessSchema(string id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
