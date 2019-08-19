3dc1a7365adf028b36a3225694f5339002898af9
Stewart Pavitt <stewart.pavitt@gmail.com>
2019-08-19 13:02:57 +0100

    Created a V1 of the project without external aliases

---
+++ Aliases/Controllers/v1/ProcessesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Schemas;
+using StorageRepository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aliases.Controllers.v1
+{
+    [ApiVersion("1.0", Deprecated =true)]
+    [Route("api/v1/processes")]
+    [ApiController]
+    public class ProcessesController : ControllerBase
+    {
+        private IDefaultStorageRepository _repo;
+
+        public ProcessesController(IDefaultStorageRepository repo) 
+        {
+            _repo = repo;
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpPost]
+        public ActionResult Write([FromBody] ProcessSchema schema)
+        {
+            _repo.Write(new v1StorageModel.StorageModel() { Contents = schema.ToString(), Id = schema.Id, VersionOfContents = 1 });
+            return Ok();
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        public ActionResult<List<ProcessSchema>> Get()
+        {
+            
+            return _repo.GetAll()
+              .Select(x => JsonConvert.DeserializeObject<ProcessSchema>(x.Contents))
+              .ToList();
+            
+        }
+
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        [Route("{id}")]
+        public ActionResult<ProcessSchema> GetById(string id)
+        {
+            return JsonConvert.DeserializeObject<ProcessSchema>(_repo.Get(id).Contents);
+        }
+    }
+}
