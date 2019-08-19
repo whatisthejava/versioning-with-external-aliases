53124bbf2d8e009fcf1f64d057638e65bb82c2a9
Stewart Pavitt <stewart.pavitt@gmail.com>
2019-08-19 13:02:57 +0100

    Created a V1 of the project without external aliases

---
+++ StorageRepository/StorageModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace v1StorageModel
+{
+    public class StorageModel
+    {
+        public string Id { get; set; }
+        public string Contents { get; set; }
+        public int VersionOfContents { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                var model = (StorageModel)obj;
+                return (this.Id == model.Id) && (this.Contents == model.Contents) && (this.VersionOfContents == model.VersionOfContents);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
